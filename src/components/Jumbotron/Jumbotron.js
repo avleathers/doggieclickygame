1428606b93c03d66a2c78377c27d5aa60680c286
Missy Leathers <missy@avleathers.com>
2019-02-05 09:48:32 -0800

    New file structure

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Jumbotron.css";
+
+
+const Jumbotron = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container text-center">
+  <br>
+  </br>
+    <h1 className="display-4">React Clicky Game!</h1>
+    <br>
+  </br>
+    <p className="lead">Click on any dog to earn points, but don't click on any dog more than once!</p>
+  </div>
+</div>
+);
+
+export default Jumbotron;
